b6f51dcad88429602f4a722f33886213ca4c0b75
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-09 16:51:28 -0700

    Implemented basic work with arguments

--- src/main.c
+++ src/main.c
@@ -6,14 +6,30 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/08 21:35:24 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/04/08 21:36:17 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/04/09 16:50:59 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include "fdf.h"
+#include <fcntl.h>
+#include <stdlib.h>
 
-int		main(void)
+void	die(int code, char *msg)
 {
-	ft_printf("fdf\n");
+	ft_printf("%s\n", msg);
+	exit(code);
+}
+
+int		main(int ac, char *av[])
+{
+	int		fd;
+	t_map	*map;
+
+	if (ac != 2)
+		die(0, "usage: ./fdf [filename]");
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0 || !read_map(fd, &map))
+		die(1, "error: invalid map");
 	return (0);
 }
