21e240f48afd96aea64c12f2459716db19e66307
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-09 19:32:55 -0700

    Refactor

--- src/main.c
+++ src/main.c
@@ -6,13 +6,14 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/08 21:35:24 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/04/09 16:50:59 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/04/09 19:30:30 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "memory.h"
 #include "fdf.h"
+#include <mlx.h>
 #include <fcntl.h>
 #include <stdlib.h>
 
@@ -41,16 +42,23 @@ t_mlx	*init_mlx(char *title)
 		del_mlx(&mlx);
 	return (mlx);
 }
+
 int		main(int ac, char *av[])
 {
 	int		fd;
 	t_map	*map;
+	t_mlx	*mlx;
 
 	map = NULL;
 	if (ac != 2)
-		die(0, "usage: ./fdf [filename]");
+		die(0, USAGE);
 	fd = open(av[1], O_RDONLY);
 	if (fd < 0 || !read_map(fd, &map))
-		die(1, "error: invalid map");
+		die(1, ERR_INVMAP);
+	mlx = init_mlx(av[1]);
+	if (mlx == NULL)
+		die(1, ERR_MLXINIT);
+	mlx->map = map;
+	mlx_loop(mlx->mlx);
 	return (0);
 }
