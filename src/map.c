c4dba6e30ef75c7694f8acfc50e8588e1d9d4209
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-10 20:50:06 -0700

    Fixed setx, sety, setz macro

--- src/map.c
+++ src/map.c
@@ -6,15 +6,12 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 14:52:37 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/04/09 16:46:38 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/04/10 20:44:46 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-#include "list.h"
-#include "memory.h"
-#include "numbers.h"
-#include "strings.h"
+#include "libft.h"
 
 t_matrix	init_matrix(int rows, int cols)
 {
@@ -28,10 +25,11 @@ t_matrix	init_matrix(int rows, int cols)
 
 int		populate_map(t_map **m, t_list *list)
 {
-	int		i;
-	int		j;
-	t_list	*head;
-	char	**split;
+	int			i;
+	int			j;
+	t_point3d	*p;
+	t_list		*head;
+	char		**split;
 
 	head = list;
 	i = -1;
@@ -40,7 +38,7 @@ int		populate_map(t_map **m, t_list *list)
 		j = -1;
 		split = ft_strsplit(list->content, ' ');
 		while (++j < (*m)->width)
-			SETXYZ((*m)->points[i * (*m)->width + j], j, i, ft_atoi(split[j]));
+			SETXYZ((p = &GET_POINT((*m), j, i)), j, i, ft_atoi(split[j]));
 		ft_free_split(split);
 		list = list->next;
 	}
