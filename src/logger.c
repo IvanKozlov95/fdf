ebeb1f3fa84a25f88e2c8da656888650ae5d6860
Ivan Kozlov <ikozlov9000@gmail.com>
2018-04-13 22:51:53 -0700

    Added print_points method

--- src/logger.c
+++ src/logger.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 16:48:59 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/04/10 16:50:11 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/04/13 22:49:56 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,22 @@
 void		log_point(t_point3d p)
 {
 	printf("Point: %f, %f, %f\n", p.x, p.y, p.z);
+}
+
+void		print_point(t_mlx *mlx, int x, int y)
+{
+	log_point(point_project(GET_POINT(mlx->map, x, y), mlx));
+}
+
+void		print_ponts(t_mlx *mlx)
+{
+	int		i;
+	int		size;
+	t_map	*map;
+
+	i = -1;
+	map = mlx->map;
+	size = map->height * map->width;
+	while (++i < size)
+		print_point(mlx, i % map->width, i / map->width);
 }
