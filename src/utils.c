7019f348a290890710ebbeb129c6997d70256f6f
Ivan Kozlov <ivaan.kozlov@gmail.com>
2018-04-15 15:52:35 -0700

    Handling the case when colors are equal

--- src/utils.c
+++ src/utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/09 16:43:15 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/04/14 22:13:36 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/04/15 13:47:25 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,8 +32,10 @@ int		get_color(int c1, int c2, double p)
 	int		b;
 	int		g;
 
-	r = LERP(c1 >> 16 & 0xFF, c2 >> 16 & 0xFF, p);
-	g = LERP(c1 >> 8 & 0xFF, c2 >> 8 & 0xFF, p);
+	if (c1 == c2)
+		return (c1);
+	r = LERP((c1 >> 16) & 0xFF, (c2 >> 16) & 0xFF, p);
+	g = LERP((c1 >> 8) & 0xFF, (c2 >> 8) & 0xFF, p);
 	b = LERP(c1 & 0xFF, c2 & 0xFF, p);
 	return (r << 16 | g << 8 | b);
 }
