e6d1f1b5aad7c1fdb6694d12e31178da4ab96c0e
Ivan Kozlov <ikozlov9000@gmail.com>
2018-05-07 12:15:54 -0700

    Fixed scale by z axes

--- src/point.c
+++ src/point.c
@@ -6,7 +6,7 @@
 /*   By: ikozlov <ikozlov@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/10 16:21:27 by ikozlov           #+#    #+#             */
-/*   Updated: 2018/04/15 20:09:13 by ikozlov          ###   ########.fr       */
+/*   Updated: 2018/05/07 12:10:33 by ikozlov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,9 +36,8 @@ t_point3d	point_project(t_point3d p, t_mlx *mlx)
 {
 	p.x -= (double)(mlx->map->width - 1) / 2.0f;
 	p.y -= (double)(mlx->map->height - 1) / 2.0f;
-	p.z = p.z == mlx->map->d_min ? 0 : p.z + (mlx->map->d_max + mlx->map->d_min) / 2.0f;
-	// p.z = p.z == 0 ? 0 : p.z - mlx->depth;
-	// p.z -= (double)(mlx->map->d_max + mlx->map->d_min) / 2.0f;
+	p.z = p.z == mlx->map->d_min ?
+		p.z : p.z + (mlx->map->d_max + mlx->map->d_min) / 2.0f;
 	p = rotate(p, mlx->cam);
 	p.x *= mlx->cam.scale;
 	p.y *= mlx->cam.scale;
